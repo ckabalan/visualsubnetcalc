89b5b6658884a64fbbaa61b1357d72c49a4341b7
Stephen Sennett <ssennett@ssennett.net>
2024-10-05 11:57:12 +1000

    Adding keyboard shortcuts for Sp[l]it and [J]oin

--- dist/js/main.js
+++ dist/js/main.js
@@ -164,6 +164,28 @@ $('#calcbody').on('click', 'td.split,td.join', function(event) {
     renderTable(operatingMode);
 })
 
+$('#calcbody').on('keydown', 'td.note input', function(event) {
+    // HTML DOM Data elements! Yay! See the `data-*` attributes of the HTML tags
+    //
+    // TODO: Fix focus
+    if (event.ctrlKey && event.key === 'l') {
+        clearTimeout(noteTimeout);
+        mutate_subnet_map('note', this.dataset.subnet, '', this.value);
+        renderTable(operatingMode);
+
+        mutate_subnet_map('split', this.dataset.subnet, '');
+        renderTable(operatingMode);
+    } else if (event.ctrlKey && event.key === 'j') {
+        clearTimeout(noteTimeout);
+        mutate_subnet_map('note', this.dataset.subnet, '', this.value);
+        renderTable(operatingMode);
+
+        let parentNetwork = get_network_parent(subnetMap, this.dataset.subnet);
+        mutate_subnet_map('join', parentNetwork, '');
+        renderTable(operatingMode);
+    }
+})
+
 $('#calcbody').on('keyup', 'td.note input', function(event) {
     // HTML DOM Data elements! Yay! See the `data-*` attributes of the HTML tags
     let delay = 1000;
@@ -357,6 +379,26 @@ function get_network_children(network, subnetTree) {
         }
     }
     return subnetList
+}
+
+
+function get_network_parent(subnetTree, targetNetwork) {
+    for (let mapKey in subnetTree) {
+        if (mapKey.startsWith('_')) { continue; }
+        if (has_network_sub_keys(subnetTree[mapKey])) {
+            let parentNetwork = get_network_parent(subnetTree[mapKey], targetNetwork)
+            if (parentNetwork !== '') {
+                return parentNetwork
+            }
+        } else {
+            if (mapKey === targetNetwork) {
+                let networkSplit = Object.keys(subnetTree)[0].split('/')
+                let parentNetwork = networkSplit[0] + '/' + (parseInt(networkSplit[1]) - 1)
+                return parentNetwork
+            }
+        }
+    }
+    return ''
 }
 
 function get_matching_network_list(network, subnetTree) {
