fa3381f29e8e28641e66822f8eb3b0c55d347485
Chris Hailes <53359287+Chris-Hailes@users.noreply.github.com>
2024-11-10 13:27:44 +1100

    implementing dark-mode
    introduced a dark-mode button, with animation, modify the required JS script and css. Have tested locally and confirmed all features are working and all expected colours look good

--- dist/js/main.js
+++ dist/js/main.js
@@ -931,3 +931,15 @@ function importConfig(text) {
 }
 
 const rgba2hex = (rgba) => `#${rgba.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/).slice(1).map((n, i) => (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n)).toString(16).padStart(2, '0').replace('NaN', '')).join('')}`
+
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.body.setAttribute('data-bs-theme', currentTheme);
+}
+
+darkModeToggle.addEventListener('click', () => {
+    let theme = document.body.getAttribute('data-bs-theme') === 'dark' ? 'light' : 'dark';
+    document.body.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+});
